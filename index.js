3dcce50cb498f8e14abb1cc34b2378458a7c3a88
Stephanie Coy <scoy028@gmail.com>
2018-09-19 13:23:03 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+let katzDeliLine = [];
+
+function takeANumber(katzDeliLine, newName) {
+  katzDeliLine.push(newName);
+  return `Welcome, ${newName}. You are number ` + (katzDeliLine.indexOf(newName)+1) + " in line.";
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  let firstName = katzDeliLine.shift();
+  return "Currently serving " + firstName + "."
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty.";
+  }
+  for (let i = 0; i < katzDeliLine.length; ++i) {
+    let curPerson = katzDeliLine[i];
+    let placeInLine = katzDeliLine.indexOf(curPerson) + 1;
+    let messageToLine = placeInLine + ". " + curPerson;
+    return "The line is currently: " + messageToLine;
+  }
+}
